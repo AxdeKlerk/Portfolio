049e4dd179d6797d84deeb4482a6f6a56bf155ea
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-07 07:17:10 +0000

    Change About model fields Register about model in admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import About
 
-# Register your models here.
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title')
+    fields = ('name', 'title', 'bio', 'profile_image', 'github_link', 'linkedin_link')
+
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,8 @@ from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     name = models.CharField(max_length=100)
-    short_bio = models.CharField(max_length=250)
-    description = models.TextField()
+    title = models.CharField(max_length=1000)
+    bio = models.TextField()
     profile_image = CloudinaryField('image', blank=True, null=True)
     github_link = models.URLField(blank=True, null=True)
     linkedin_link = models.URLField(blank=True, null=True)
