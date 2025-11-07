685ae2eb88b58003076532bb50466368dc4bf29c
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-07 09:12:02 +0000

    Update portfolio files, keep .env ignored

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import About
 
-# Create your views here.
+def about_view(request):
+    about = About.objects.first()
+    return render(request, 'portfolio/about.html', {'about': about})
+
+
