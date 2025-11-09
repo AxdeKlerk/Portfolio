65ee7b21571a9ec1ed4400745ca07379079e9e50
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-09 14:04:36 +0000

    Add navbar to base template Add hero section to home template Add all views and url paths Re-order text in about page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,8 @@ app_name = 'portfolio'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('blog/', views.blog, name='blog'),
+    path('cv/', views.cv, name='cv'),
+    path('contact/', views.contact, name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from .models import About
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    return render(request, 'portfolio/home.html', {'page': 'home'})
 
 def about(request):
     about = About.objects.first()
-    return render(request, 'portfolio/about.html', {'about': about})
+    return render(request, 'portfolio/about.html', {'page': 'about', 'about': about})
 
+def projects(request):
+    return render(request, 'portfolio/projects.html', {'page': 'projects'})
 
+def blog(request):
+    return render(request, 'portfolio/blog.html', {'page': 'blog'})
+
+def cv(request):
+    return render(request, 'portfolio/cv.html', {'page': 'cv'})
+
+def contact(request):
+    return render(request, 'portfolio/contact.html', {'page': 'contact'})
