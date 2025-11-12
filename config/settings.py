d19b544b972dbbcfd3102c7a64e37d7d988d3ce2
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-12 12:24:09 +0000

    Fix STATIC_URL path for static files

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
