44a0a9d45814305fd7f2ac9e1c7451780ee97f74
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-18 21:46:43 +0000

    Temporarily disable About context processor for Render access

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'portfolio.context_processors.about_data',
+                # 'portfolio.context_processors.about_data',
             ],
         },
     },
